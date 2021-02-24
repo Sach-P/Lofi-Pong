f622987a86aec48297d753b8479f218e8f8901d8
nstCode <bassnoaht@gmail.com>
2021-02-24 11:04:13 -0600

    Update Lo-FiPong.py

--- Lo-FiPong.py
+++ Lo-FiPong.py
@@ -1,2 +1,15 @@
 import pygame 
+<<<<<<< Updated upstream
 import random
+=======
+import random
+
+from pygame.locals import (
+    K_D,
+    K_S,
+    K_A,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+>>>>>>> Stashed changes
