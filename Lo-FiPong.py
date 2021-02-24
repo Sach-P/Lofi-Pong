64f91c73d898f20f4a6abbfae8df3e88912437c5
nstCode <bassnoaht@gmail.com>
2021-02-24 11:17:51 -0600

    Update Lo-FiPong.py

--- Lo-FiPong.py
+++ Lo-FiPong.py
@@ -9,3 +9,12 @@ from pygame.locals import (
 	QUIT,
 	KEYDOWN,
 )
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+
+#FIXME: add a player and enemy class
+
+pygame.itit()
+
+screen = pygame.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
