724013d5b565a4d9dcb82a00f7cfc9e7bd0e2c18
nstCode <bassnoaht@gmail.com>
2021-04-14 10:51:55 -0500

    Update Lo-FiPong.py

--- Lo-FiPong.py
+++ Lo-FiPong.py
@@ -6,10 +6,17 @@ from pygame.locals import(
     K_a,
     K_s,
     K_d,
+    K_LEFT,
+    K_RIGHT,
+    K_DOWN,
+    K_UP,
     K_ESCAPE,
     KEYDOWN,
     QUIT,
 )
+
+f = open('timeBtwnBeats.txt', 'r')
+FRAME_RATE = 60
 
 class Table(pygame.sprite.Sprite):
     def __init__(self):
@@ -23,67 +30,106 @@ class Player(pygame.sprite.Sprite):
         super(Player, self).__init__()
         self.surf = pygame.Surface((75, 35))
         self.surf.fill((255, 230, 255))
-        self.rect = self.surf.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50))
+        self.Lx = SCREEN_WIDTH / 2 - 150
+        self.Mx = SCREEN_WIDTH / 2
+        self.Rx = SCREEN_WIDTH / 2 + 150
+        self.y = SCREEN_HEIGHT - 85
+        self.rect = self.surf.get_rect(center=(self.Mx, self.y))
 
     def update(self, pressed_keys):
-        if pressed_keys[K_a]:
-            self.rect = self.surf.get_rect(center=(SCREEN_WIDTH / 2 - 200, SCREEN_HEIGHT - 50))
-        if pressed_keys[K_s]:
-            self.rect = self.surf.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50))
-        if pressed_keys[K_d]:
-            self.rect = self.surf.get_rect(center=(SCREEN_WIDTH / 2 + 200, SCREEN_HEIGHT - 50))
+        if pressed_keys[K_a] or pressed_keys[K_LEFT]:
+            self.rect = self.surf.get_rect(center=(self.Lx, self.y))
+        if pressed_keys[K_s] or pressed_keys[K_DOWN] or pressed_keys[K_UP]:
+            self.rect = self.surf.get_rect(center=(self.Mx, self.y))
+        if pressed_keys[K_d] or pressed_keys[K_RIGHT]:
+            self.rect = self.surf.get_rect(center=(self.Rx, self.y))
+
+class Opponent(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Opponent, self).__init__()
+        self.surf = pygame.Surface((75, 35))
+        self.surf.fill((255, 230, 255))
+        self.Lx = SCREEN_WIDTH / 2 - 150
+        self.Mx = SCREEN_WIDTH / 2
+        self.Rx = SCREEN_WIDTH / 2 + 150
+        self.y = 85
+        self.rect = self.surf.get_rect(center=(self.Mx, self.y))
+
+    def update(self):
+        if ball.endOfFile and ball.centerY < (SCREEN_HEIGHT / 2)  and ball.toY == ball.tY:
+            self.rect = self.surf.get_rect(center=(ball.toX - 75, 85))
+
+        elif ball.centerY < (SCREEN_HEIGHT / 2) and ball.toY == ball.tY:
+            self.rect = self.surf.get_rect(center=(ball.toX, 85))
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         super(Ball, self).__init__()
         self.surf = pygame.Surface((20, 20))
         self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect(center=(SCREEN_WIDTH / 2, 0))
+        self.rect = self.surf.get_rect(center=(SCREEN_WIDTH / 2, 100))
+
         self.bY = SCREEN_HEIGHT - 100
         self.tY = 100
 
-        self.Lx = SCREEN_WIDTH / 2 - 200
+        self.Lx = SCREEN_WIDTH / 2 - 150
         self.Mx = SCREEN_WIDTH / 2
-        self.Rx = SCREEN_WIDTH / 2 + 200
+        self.Rx = SCREEN_WIDTH / 2 + 150
 
         self.toY = 0
         self.toX = 0
         self.dx = 0
         self.dy = 0
-      
 
+        self.r = random.randint(1, 3)
+
+        self.mult = 0
+        self.endOfFile = False
+      
     def update(self):
 
-        centerX, centerY = self.rect.center
+        self.centerX, self.centerY = self.rect.center
 
-        r = random.randint(1, 3)
+        self.simulate()
 
-        if r == 1:
+        if self.r == 1:
             self.toX = self.Lx
-        elif r == 2:
+        elif self.r == 2:
             self.toX = self.Mx
-        elif r == 3:
+        elif self.r == 3:
             self.toX = self.Rx
-
-        if centerY <= self.tY:
-            self.toY = self.bY
-            if (self.toX - centerX != 0):
-                self.dx = (self.toX - centerX)/math.sqrt((self.toX - centerX)*(self.toX - centerX) + (self.toY - centerY)*(self.toY - centerY))
-            if (self.toY - centerY != 0):
-                self.dy = 1
-    
-
         
+        if (self.toX - self.centerX != 0):
+            self.dx = (self.toX - self.centerX) / abs(self.toY - self.centerY)
 
-        if centerY >= self.bY:
+        self.rect.move_ip(self.dx * self.mult, self.dy * self.mult)  
+
+    def simulate(self):
+
+        opponent.update()
+            
+        if self.centerY <= self.tY and pygame.sprite.collide_rect(self, opponent):
+            self.r = random.randint(1, 3)
+            self.toY = self.bY
+            next_line = f.readline()
+            if next_line:
+                self.mult = abs(self.toY - self.centerY) / (FRAME_RATE * float(next_line))
+            else:
+                self.endOfFile = True
+            if (self.toY - self.centerY != 0):
+                self.dy = 1
+        
+        elif self.centerY >= self.bY and pygame.sprite.collide_rect(self, player):
+            self.r = random.randint(1, 3)
             self.toY = self.tY
-         if (self.toX - centerX != 0):
-                self.dx = (self.toX - centerX)/math.sqrt(self.toX**2 + centerX**2)
-            if (self.toY - centerY != 0):
+            next_line = f.readline()
+            if next_line:
+                self.mult = abs(self.toY - self.centerY) / (FRAME_RATE * float(next_line))
+            else:
+                self.endOfFile = True
+            if (self.toY - self.centerY != 0):
                 self.dy = -1
-
-
-        self.rect.move_ip(self.dx * 10, self.dy * 10)   
+  
 
 pygame.init()
 
@@ -107,8 +153,8 @@ player = Player()
 entities.add(player)
 ball = Ball()
 entities.add(ball)
-
-
+opponent = Opponent()
+entities.add(opponent)
 
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
@@ -124,8 +170,6 @@ while running:
                 
         if event.type == pygame.QUIT:
             running = False
-
-    pressed_keys = pygame.key.get_pressed()
 
     screen.fill((colorChangeRed, colorChangeGreen, colorChangeBlue))
 
@@ -159,14 +203,25 @@ while running:
         colorChangeRed += colorIncrementRed
         colorIncrementBlue = 2
 
+    pressed_keys = pygame.key.get_pressed()
+
     player.update(pressed_keys)
     ball.update()
+
+    if ball.endOfFile == True and ball.centerY < 0:
+        print("You Win!")
+        running = False
+
+    elif ball.centerY > SCREEN_HEIGHT:
+        print("You Lose :(")
+        running = False
 
     for entity in entities:
         screen.blit(entity.surf, entity.rect)
 
     pygame.display.flip()
 
-    clock.tick(30)
+    clock.tick(FRAME_RATE)
 
+f.close()
 pygame.quit()
